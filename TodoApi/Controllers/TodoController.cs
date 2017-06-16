59271016da4d925fd5f3d220c4e0dc884da08904
FlyLive <1585213801@qq.com>
2017-06-16 16:02:04 +0800

    Todo

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Todo")]
+    public class TodoController : Controller
+    {
+        public TodoController(ITodoRepository todoItems)
+        {
+            TodoItems = todoItems;
+        }
+
+        public ITodoRepository TodoItems { get; set; }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return TodoItems.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+    }
+}
